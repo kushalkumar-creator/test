cdfe3f3362bb63f4599076222aa79caf3841f8a1
kushalkumar-creator <kushalkumar5859@gmail.com>
2026-03-29 19:00:15 +0530

    add express server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello from deployed app 🚀");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Your app is live!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
